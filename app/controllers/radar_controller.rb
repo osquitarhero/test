a9af6936b870377250d07461092bf9fda57dc2ba
oscar <oscar@imsai8080.home>
2024-01-25 16:51:33 +0100

    fix wrong parameters request

--- app/controllers/radar_controller.rb
+++ app/controllers/radar_controller.rb
@@ -10,26 +10,33 @@ class RadarController < ApplicationController
         
     # TODO permit only safe params
     #courses_params = params.permit(:criteria, editions: [:date, courses: [:name, :type]  ])
+    
+    if params[:criteria] && params[:editions]
 
-    radar = Radar.new( params[:criteria], params[:editions])
+      radar = Radar.new( params[:criteria], params[:editions])
     
-    # create a new radar class with criteria and editions
-    logger.info "#{radar.criteria_list.count } criteria:"
-    logger.info radar.criteria_list
+      # create a new radar class with criteria and editions
+      logger.info "#{radar.criteria_list.count } criteria:"
+      logger.info radar.criteria_list
     
-    logger.info "#{radar.available_editions.count } avaliable editions:"
-    logger.info radar.available_editions
+      logger.info "#{radar.available_editions.count } avaliable editions:"
+      logger.info radar.available_editions
   
     
-    if radar.run 
-      logger.info "#{radar.editions.count} retrieved courses"
-      logger.info radar.editions
+      if radar.run 
+        logger.info "#{radar.editions.count} retrieved courses"
+        logger.info radar.editions
       
-      render json: radar.editions, status: 200  
-    else 
-      # TODO include the error message in the response
-      render status: 500
+        render json: radar.editions, status: 200  
+      else 
+        # TODO include the error message in the response
+        render status: 500
+      end
+      
+    else
+      render json: {error: "invalid params"}, status: 404
     end
+    
   end
   
 end
--- spec/requests/radar_spec.rb
+++ spec/requests/radar_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe "Radars", type: :request do
   describe "POST /radar" do
     it "returns http success" do
-      post "/radar/"
+      post "/radar/", params: {criteria: "closest", editions: [] } 
       expect(response).to have_http_status(:success)
+    end
+
+    it "returns http missing due bad or lack of params" do
+      post "/radar/", params: {criteria: "closest"}
+      expect(response).to have_http_status(:missing)
     end
 
   end
